486fb1f6dcd478606ea4be8384691993f36e61a5
EMJzero <marcoron00@gmail.com>
2023-01-14 22:43:11 +0100

    DBAccess added Emsp

---
+++ src/cpms/backend/src/model/Emsp.ts
@@ -0,0 +1,24 @@
+import { DBAccess } from "../DBAccess";
+import { FieldPacket, RowDataPacket } from "mysql2/promise";
+
+export class Emsp {
+    id: number;
+    APIKey: string;
+
+    constructor(id: number, APIKey: string) {
+        this.id = id;
+        this.APIKey = APIKey;
+    }
+
+    private static async checkCredentials(APIKey: string): Promise<boolean> {
+        const connection = await DBAccess.getConnection();
+
+        const [result, _]: [RowDataPacket[], FieldPacket[]] = await connection.execute(
+            "SELECT id FROM emsps WHERE APIKey = ?",
+            [APIKey]);
+
+        connection.release();
+
+        return result.length != 0;
+    }
+}
